ec4a6cc2145ec4bffe81183049562f1a39aae2da
humayun0712 <113867509+humayun0712@users.noreply.github.com>
2024-05-28 16:06:40 +0500

    Read Content form the file

---
+++ correct_code.md
@@ -0,0 +1,10 @@
+## Reading Content form the file
+  ifstream textfile;   //creating object of the fstream object
+
+
+    textfile.open ("file.txt"); //open file
+    stringstream buffer_holder;
+    buffer_holder << textfile.rdbuf(); //read all the file content at once
+    string text = buffer_holder.str(); //coverting and storing that content into string
+    textfile.close();//closing the file
+    cout<<text<<endl;
--- frequency.cpp
+++ frequency.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <sstream> //to read file into stream
 using namespace std;
 
 void frequencies(char words[], int fre[])
@@ -8,8 +10,15 @@ void frequencies(char words[], int fre[])
 
 int main()
 {
-    char aplhabets[97];
+        char aplhabets[97];
+    int freq [97];
+    ifstream textfile;   //creating object of the fstream object
 
-    
 
+    textfile.open ("file.txt"); //open file
+    stringstream buffer_holder;
+    buffer_holder << textfile.rdbuf(); //read all the file content at once
+    string text = buffer_holder.str(); //coverting and storing that content into string
+    textfile.close();//closing the file
+    cout<<text<<endl;
 }
